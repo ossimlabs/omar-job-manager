81b57baad92161f12800c8f6adaae9872217e33d
Tyler Culp <tylercculp@gmail.com>
2019-01-07 10:01:35 -0500

    Removed application.name from application.yml

--- omar-task-bucket-scan/src/main/groovy/omar/job/manager/task/bucket/scan/BucketScan.groovy
+++ omar-task-bucket-scan/src/main/groovy/omar/job/manager/task/bucket/scan/BucketScan.groovy
@@ -1,23 +1,26 @@
 package omar.job.manager.task.bucket.scan
 
 import com.amazonaws.auth.AWSCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.amazonaws.services.s3.model.ListObjectsV2Request
 import com.amazonaws.services.s3.model.ListObjectsV2Result
 import org.springframework.boot.CommandLineRunner
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class BucketScanTask implements CommandLineRunner
 {
 
-	/**
-	 * Autowired AWSCredentialsProvider used to authenticate the s3Client object with an AWS account
-	 */
-	@Autowired
-	AWSCredentialsProvider awsCredentialsProvider
+	@Value('${cloud.aws.credentials.accessKey}')
+	String accessKey
+
+	@Value('${cloud.aws.credentials.secretKey}')
+	String secretKey
+
+	BasicAWSCredentials awsCredentials = BasicAWSCredentials(accessKey, secretKey)
 
 	@Override
 	void run( String... strings ) throws Exception
@@ -28,7 +31,7 @@ class BucketScanTask implements CommandLineRunner
 		def delimiter = '/'
 		
 		AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-			.withCredentials( awsCredentialsProvider )
+			.withCredentials( awsCredentials )
 			.withRegion( clientRegion )
 			.build()
 		
